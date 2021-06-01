9ccc4e5cc3639529a6a7bb2ab2f7828970227736
Enis Yesilbas <yeen1011@hs-karlsruhe.de>
2021-06-01 14:37:21 +0200

    Implementierung der Entity Nachname

---
+++ src/app/buch/buch-routing.module.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { AdminGuard } from '../auth/admin.guard';
+import { BalkendiagrammComponent } from './diagramme/balkendiagramm.component';
+import { CreateBuchComponent } from './create-buch/create-buch.component';
+import { CreateBuchGuard } from './create-buch/create-buch.guard';
+import { DetailsBuchComponent } from './details-buch/details-buch.component';
+import { LiniendiagrammComponent } from './diagramme/liniendiagramm.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import type { Routes } from '@angular/router';
+import { SucheBuecherComponent } from './suche-buecher/suche-buecher.component';
+import { TortendiagrammComponent } from './diagramme/tortendiagramm.component';
+import { UpdateBuchComponent } from './update-buch/update-buch.component';
+
+// Route-Definitionen fuer das Feature-Modul "buch":
+// Zuordnung von Pfaden und Komponenten mit HTML-Templates
+const routes: Routes = [
+    {
+        path: 'suche',
+        component: SucheBuecherComponent,
+    },
+    {
+        path: 'create',
+        component: CreateBuchComponent,
+        canActivate: [AdminGuard],
+        canDeactivate: [CreateBuchGuard],
+    },
+    {
+        path: 'balkendiagramm',
+        component: BalkendiagrammComponent,
+        canActivate: [AdminGuard],
+    },
+    {
+        path: 'liniendiagramm',
+        component: LiniendiagrammComponent,
+        canActivate: [AdminGuard],
+    },
+    {
+        path: 'tortendiagramm',
+        component: TortendiagrammComponent,
+        canActivate: [AdminGuard],
+    },
+
+    // id als Pfad-Parameter
+    {
+        path: ':id',
+        component: DetailsBuchComponent,
+    },
+    {
+        path: 'details',
+        component: DetailsBuchComponent,
+    },
+    {
+        path: ':id/update',
+        component: UpdateBuchComponent,
+        canActivate: [AdminGuard],
+    },
+];
+
+@NgModule({
+    exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+})
+export class BuchRoutingModule {}
