c01b8beb4cd10a280f9e6a731b9e04c7fff36c0e
Florian Göbel <florian.goebel@outlook.de>
2021-09-07 20:25:33 +0200

    Fixed material inputs for suchformular

--- src/app/buch/suche-buch/suchformular/suche-rating.module.ts
+++ src/app/buch/suche-buch/suchformular/suche-rating.module.ts
@@ -15,13 +15,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { SucheRatingComponent } from './suche-rating.component';
 
 @NgModule({
     declarations: [SucheRatingComponent],
     exports: [SucheRatingComponent],
-    imports: [FormsModule],
+    imports: [
+        FormsModule,
+        CommonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+    ],
 })
 export class SucheRatingModule {}
